198cccf801d7de50058f7cabfd9e49a6e0b72fa2
unknown <annakrav07@mail.ru>
2026-04-29 14:37:12 +0500

    3

--- project1/routes/auth.js
+++ project1/routes/auth.js
@@ -5,6 +5,10 @@ router.post('/register', (req,res) => {
     if (!fio || !email || !password)  {
        return res.send('Заполните все поля'); 
     }
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/;
+    if(!passwordRegex.test(password)) {
+        return res.send('Пароль не подходит требованиям');
+    }
     res.send('Регистрация завершена')
 });
 module.exports = router;
